b8bfe5e2024f74790e366f0aad6383e76665d375
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-11-03 16:23:56 +0300

    New files added

---
+++ pages/auth_page.py
@@ -0,0 +1,18 @@
+import time
+
+from .base_page import BasePage
+from .locators import AuthLocators
+from time import sleep
+import os
+
+
+class AuthPage(BasePage):
+
+    def __init__(self, driver, timeout=10):
+        super().__init__(driver, timeout)
+        url = os.getenv("LOGIN_URL") or "https://petfriends.skillfactory.ru/login"
+        driver.get(url)
+        self.email = driver.find_element(*AuthLocators.AUTH_EMAIL)
+        self.passw = driver.find_element(*AuthLocators.AUTH_PASS)
+        self.btn = driver.find_element(*AuthLocators.AUTH_BTN)
+        time.sleep(2)
