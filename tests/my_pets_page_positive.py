a45f76b330d0a65fcbbf4c0ed566a03b3de684a9
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-11-14 18:44:07 +0300

    File updated

--- tests/my_pets_page_positive.py
+++ tests/my_pets_page_positive.py
@@ -5,8 +5,8 @@ from colorama import Fore, Style
 import time
 
 
-class TestMyPetsPageCreate:
-    @pytest.mark.one
+class TestMyPetsPage:
+    @pytest.mark.create_simple
     def test_create_pet_simple_positive(self, driver):
 
         page = MyPetsPage(driver)
@@ -25,6 +25,7 @@ class TestMyPetsPageCreate:
             page.make_screenshot()
             print(Style.DIM + Fore.GREEN + f"\nКарточка питомца успешно создана!")
 
+    @pytest.mark.create_wth_photo
     def test_create_pet_wth_photo_positive(self, driver):
         """Позитивный тест проверки создания карточки питомца с фото. Валидация теста выполнена успешно в случае, если
          после ввода всех необходимых данных в форму карточки, пользователь остается на страницы с эндпоинтом
@@ -46,6 +47,7 @@ class TestMyPetsPageCreate:
             assert page.get_relative_link() == "/my_pets"
             print(Style.DIM + Fore.GREEN + f"\nКарточка питомца успешно создана!")
 
+    @pytest.mark.create_pairwise
     @pytest.mark.parametrize("photo", [photo_1_jpg, photo_2_jpg], ids=["photo_jpeg_>100kb", "photo_jpeg_<100kb"])
     @pytest.mark.parametrize("name", [russian_chars(), latin_chars()], ids=["cyrillic chars", "latin chars"])
     @pytest.mark.parametrize("breed", [latin_chars(), russian_chars()], ids=["latin chars", "cyrillic chars"])
@@ -55,7 +57,7 @@ class TestMyPetsPageCreate:
         спецификации. Реализована техника попарного тестирования Pairwise. Валидация каждого теста выполнена успешно в
         случае, если после ввода всех необходимых данных в форму карточки, пользователь остается на страницы с
         эндпоинтом "/my_pets", а каждая сгенерированная карточка отображается в стеке питомцев пользователя со всеми
-        указанными данными."""
+        переданными через фикстуру данными."""
 
         page = MyPetsPage(driver)
         page.add_pet_btn_click()
@@ -72,6 +74,7 @@ class TestMyPetsPageCreate:
             assert page.get_relative_link() == "/my_pets"
             print(Style.DIM + Fore.GREEN + f"\nКарточка питомца успешно создана!")
 
+    @pytest.mark.delete_pet
     def test_delete_pet_positive(self, driver):
         """Позитивный тест проверки удаления пользователем ранее созданной им карточки питомца. Валидация теста
         выполнена успешно в случае, если после нажатия на элемент "Удалить питомца" в карточке питомца, указанная
