c7d9d83ecc2f0edd10b868397e618a7266081de4
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-11-15 17:56:12 +0300

    File updated

--- tests/test_auth_positive.py
+++ tests/test_auth_positive.py
@@ -1,17 +1,12 @@
 from pages.auth_page import AuthPage
-from selenium import webdriver
 from settings import *
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-driver = webdriver.Chrome(options=options)
-
 
 class TestPetFriendsPages:
-
-    def test_auth_page(self):
-        page = AuthPage(driver)
+    
+    def test_auth_page(self, auth_driver):
+        page = AuthPage(auth_driver)
         page.enter_email(email)
         page.enter_pass(password)
         page.btn_click()
